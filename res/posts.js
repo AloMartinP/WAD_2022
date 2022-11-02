8b5981c9ef2631cd40562aec376cfcd2d66abd55
AloMartinP <73949637+AloMartinP@users.noreply.github.com>
2022-11-02 22:06:04 +0200

    fetch json

--- res/posts.js
+++ res/posts.js
@@ -1,6 +1,57 @@
-[
-    {
-        id: 1,
-        text:"lorem"
+window.onload = function () {
+    fetch('https://api.npoint.io/b2fe586642a328d6895b')
+        .then((response) =>response.json())
+        .then(json => {
+            console.log(json);
+            displayPosts(json);
+        }).catch(err => {
+            console.log(err)
+        }).finally(() => {
+            let footer = document.createElement("footer");
+            footer.className = 'main-footer';
+            let div = document.createElement('div')
+            div.className = 'footer-nav';
+            footer.append(div);
+            let link = document.createElement('a');
+            link.innerText = 'Link 1';
+            footer.append(link);
+    })
+}
+
+function displayPosts(json) {
+    document.querySelector('.main-footer').style.display = 'none';
+    for (let post of json) {
+        const div = document.createElement('div');
+        div.className = 'post';
+
+        const headDiv = document.createElement('div');
+        headDiv.className = 'post-head';
+
+        const author = document.createElement('p');
+        author.className = 'author';
+        author.innerText = post.author;
+        const date = document.createElement('p');
+        date.innerText = post.time;
+        headDiv.append(author,date);
+
+        const img = document.createElement('img');
+        img.src = post.img;
+
+        const text = document.createElement('p');
+        text.innerText = post.text;
+
+        const button = document.createElement('button');
+        button.type = 'submit';
+
+        const buttonIcon = document.createElement('i');
+        buttonIcon.className = 'fa-regular fa-thumbs-up fa-xl';
+        button.appendChild(buttonIcon);
+
+        div.append(headDiv,img,text,button);
+
+
+        document.getElementById('main').append(div);
+        document.querySelector('.main-footer').style.display = '';
+
     }
-]
+}
